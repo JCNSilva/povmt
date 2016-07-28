8ef972a5545a2243ceef2d65c17a5ec5e447086d
Lucas Matos de Medeiros <lucas.medeiros@ccc.ufcg.edu.br>
2016-07-27 22:41:08 -0300

    salvando usuario no servidor

--- app/src/main/java/les/ufcg/edu/br/povmt/activities/SplashActivity.java
+++ app/src/main/java/les/ufcg/edu/br/povmt/activities/SplashActivity.java
@@ -1,12 +1,15 @@
 package les.ufcg.edu.br.povmt.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -18,17 +21,36 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import les.ufcg.edu.br.povmt.R;
 import les.ufcg.edu.br.povmt.database.UsuarioPersister;
 import les.ufcg.edu.br.povmt.models.Usuario;
+import les.ufcg.edu.br.povmt.utils.ServiceHandler;
 
 public class SplashActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
-
+    Usuario user;
     public static final String PREFERENCE_NAME = "USER_PREFERENCE";
     public static final String USER_NOME = "USER_NOME";
     public static final String USER_EMAIL = "USER_EMAIL";
     public static final String USER_URL_PHOTO = "USER_URL_PHOTO";
     public static final String USER_ID = "USER_ID";
+    private static String url = "http://lucasmatos.pythonanywhere.com/povmt/";
+    List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(2);
 
     private static final int RC_SIGN_IN = 9001;
     private static final String TAG = "SplashActivity";
@@ -96,7 +118,7 @@ public class SplashActivity extends AppCompatActivity implements GoogleApiClient
             editor.putString(USER_NOME, nome);
             editor.putString(USER_EMAIL, email);
             editor.putString(USER_ID, id_user);
-            Usuario user;
+
             if (foto_url != null) {
                 editor.putString(USER_URL_PHOTO, foto_url.toString());
                 user = new Usuario(id_user, nome, email, foto_url.toString());
@@ -111,7 +133,7 @@ public class SplashActivity extends AppCompatActivity implements GoogleApiClient
                 userPersister.inserirUsuario(user);
             }
             editor.apply();
-
+            new HttpAsyncTask().execute(url);
             updateUI(true);
         } else {
             // Signed out, show unauthenticated UI.
@@ -173,5 +195,88 @@ public class SplashActivity extends AppCompatActivity implements GoogleApiClient
             handleSignInResult(result);
         }
     }
+    public static String POST(String url, Usuario user){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+
+            String json = "";
+
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("id", user.getId());
+            jsonObject.accumulate("nome", user.getNome());
+            jsonObject.accumulate("email", user.getEmail());
+            jsonObject.accumulate("url_foto", user.getUrl());
+
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // 5. set json to StringEntity
+            StringEntity se = new StringEntity(json);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // 10. convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            return POST(urls[0], user);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
 
 }
